6270c7bf30f8a3299da9ea21287dffe4b8474584
Tim Bogdanov <timbogdanov@Dmitriys-MBP.localdomain>
2021-07-30 09:32:18 -0700

    initial commit, set up laravel and react

---
+++ resources/js/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      This is the App
+    </div>
+  )
+}
+
+export default App
